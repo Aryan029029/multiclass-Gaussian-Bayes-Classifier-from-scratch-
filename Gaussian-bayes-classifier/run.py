097af957cba845a0a8f6ebf545665f94c840dd91
Aryan Sheoran <aryansheoran319@gmail.com>
2026-03-06 18:06:47 +0530

    fixed Fixed run.py path issue and successfully ran Gaussian Bayes classifier

--- Gaussian-bayes-classifier/run.py
+++ Gaussian-bayes-classifier/run.py
@@ -1,25 +1,18 @@
 import numpy as np
-from src.bayes_classifier import GaussianBayesClassifier
+import sys
+import os
 
+sys.path.append("src")
 
-# Load training data
-X_train = np.genfromtxt("data/X_train-3.csv", delimiter=",")
-y_train = np.genfromtxt("data/y_train-2.csv")
+from bayes_classifier import GaussianBayesClassifier
 
-# Load test data
-X_test = np.genfromtxt("data/X_test_all-1.csv", delimiter=",")
+X_train = np.genfromtxt("Data/X_train-3.csv", delimiter=",")
+y_train = np.genfromtxt("Data/y_train-2.csv", delimiter=",")
+X_test = np.genfromtxt("Data/X_test_all-1.csv", delimiter=",")
 
-
-# Train classifier
 model = GaussianBayesClassifier()
 model.fit(X_train, y_train)
 
-
-# Predict test labels
 predictions = model.predict(X_test)
 
-
-# Save predictions
-np.savetxt("results/predictions.csv", predictions, fmt="%d", delimiter=",")
-
-print("Predictions saved to results/predictions.csv")
+print(predictions)
